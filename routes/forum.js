5e965d6b04dc43d5a6117f3e7beb9a05895c5b61
Kevin Luk <khl7wh@virginia.edu>
2020-12-14 01:58:45 -0500

    Added show view

--- routes/forum.js
+++ routes/forum.js
@@ -7,13 +7,16 @@ router.get('/', async (req, res) => {
     res.render('forum', { posts: posts });
 });
 
-// router.get('/:id', (req, res) => {
-//     res.send(req.params.id)
-// })
-
 router.get('/post', (req, res) => {
     res.render('post', { post: new Post() });
 });
+
+
+router.get('/:id', async (req, res) => {
+    const articleId = req.params.id
+    const post = await Post.findById(articleId)
+    res.render('show', { post: post })
+})
 
 router.post('/', async (req, res) => {
     let post = new Post({
@@ -25,8 +28,7 @@ router.post('/', async (req, res) => {
 
     try {
         post = await post.save();
-        // res.redirect(`/map/${post.id}`);
-        res.redirect('/forum')
+        res.redirect(`/forum/${post.id}`);
     } catch (e) {
         console.log(e);
         res.render('forum/post', { post: post });
