c79ed266b9da0ae842f4adbc3ac6cbcc7a7be3d1
Kevin Luk <khl7wh@virginia.edu>
2020-12-14 02:37:16 -0500

    Added requirement for Post model

--- Models/Post.js
+++ Models/Post.js
@@ -11,6 +11,7 @@ const postSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now,
+        required: true,
     },
     description: {
         type: String,
